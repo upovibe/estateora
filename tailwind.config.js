dc677a0298b682981446047ecb5dc6b8874fc7f6
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-31 00:48:36 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#0061ff0A", // 10% opacity
+          200: "#0061ff1A", // 20% opacity
+          300: "#0061ff", // Solid color
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        dark: {
+          DEFAULT: "#000000", // Corrected typo
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
+    },
   },
   plugins: [],
-}
+};
