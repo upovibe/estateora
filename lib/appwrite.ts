7868e7f8467ec0d2d6678d14d3e2ac88301ca1df
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-31 15:04:44 +0000

    `Update Appwrite configuration platform ID`

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -12,7 +12,7 @@ import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-  platform: "com.jsm.restate",
+  platform: "com.upodev.estateora",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
