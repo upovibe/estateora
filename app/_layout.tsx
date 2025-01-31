451df96ddbbf9396afa71b8efee015b79ce13eae
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-31 14:52:59 +0000

    `Update import statements and add missing import in app/_layout.tsx`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
-import { SplashScreen, Stack } from "expo-router";
-import "./global.css";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import "./global.css";
 import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
@@ -26,7 +28,7 @@ export default function RootLayout() {
 
   return (
     <GlobalProvider>
-      <Stack screenOptions={{headerShown: false}} />
+      <Stack screenOptions={{ headerShown: false }} />
     </GlobalProvider>
   );
 }
