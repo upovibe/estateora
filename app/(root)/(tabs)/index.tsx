1e8206b8045c06b441e5ed5cfa6f14310eb62c5b
upovibe <uzorpromiseokwudiri@gmail.com>
2025-02-09 18:37:14 +0000

    `feat: updated tabs/index.tsx and added use-debounce dependency`

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,32 @@
+import SearchBar from "@/components/SearchBar";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold mb-10 font-rubik text-3xl">Welcome to the App!</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between my-5">
+          <View className="flex flex-row items-center gap-2">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start">
+              <Text className="text-sm font-rubik text-black/50">
+                Good Morning
+              </Text>
+              <Text className="text-base text-gray-90 font-rubik-medium">
+                User
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity className="flex flex-row items-center bg-gray-200 rounded-full p-2">
+            <Image source={icons.bell} className="size-5" />
+          </TouchableOpacity>
+        </View>
+        <SearchBar/>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { View, TouchableOpacity, Image, TextInput } from "react-native";
+import { useDebouncedCallback } from "use-debounce";
+
+import icons from "@/constants/icons";
+import { useLocalSearchParams, router, usePathname } from "expo-router";
+
+const SearchBar = () => {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query);
+
+  const debouncedSearch = useDebouncedCallback((text: string) => {
+    router.setParams({ query: text });
+  }, 500);
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+    debouncedSearch(text);
+  };
+
+  return (
+    <View className="flex flex-row items-center justify-between w-full px-2 rounded-xl bg-gray-100 border border-primary-100 py-[7px]">
+      <View className="flex-1 flex flex-row items-center justify-start z-50">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search for anything"
+          className="text-sm font-rubik text-black-300 ml-2 flex-1"
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Image source={icons.filter} className="size-5" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchBar;
